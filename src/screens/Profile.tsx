8ff95ea5f2d62e803ee5a4b242f27ec6b17c992c
luismda <luismigueldutraalves@gmail.com>
2023-08-05 11:34:32 -0300

    feat: add image picker to select profile photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -7,17 +7,70 @@ import {
   Skeleton,
   Text,
   Heading,
+  useToast,
 } from 'native-base'
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
 
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { Input } from '@components/Input'
 import { Button } from '@components/Button'
+import { Input } from '@components/Input'
 
 const PHOTO_SIZE = 33
+const UPLOAD_PHOTO_MAX_MB_SIZE = 5
 
 export function Profile() {
-  const [hasPhotoLoaded, setHasPhotoLoaded] = useState(false)
+  const [hasPhotoLoaded, setHasPhotoLoaded] = useState(true)
+  const [userPhoto, setUserPhoto] = useState('https://github.com/luismda.png')
+
+  const toast = useToast()
+
+  async function handleSelectUserPhoto() {
+    setHasPhotoLoaded(false)
+
+    try {
+      const selectedPhoto = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (selectedPhoto.canceled) {
+        return
+      }
+
+      const [photo] = selectedPhoto.assets
+
+      if (photo.uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photo.uri)
+
+        const photoSizeInBytes = photoInfo.exists ? photoInfo.size : 0
+        const photoSizeInMegaBytes = photoSizeInBytes / 1024 / 1024
+
+        if (photoSizeInMegaBytes > UPLOAD_PHOTO_MAX_MB_SIZE) {
+          toast.show({
+            title: `Foto muito grande! Escolha uma de até ${UPLOAD_PHOTO_MAX_MB_SIZE}MB.`,
+            placement: 'top',
+            bgColor: 'red.500',
+          })
+
+          return
+        }
+
+        setUserPhoto(photo.uri)
+      }
+    } catch (error) {
+      toast.show({
+        title: 'Ocorreu um erro ao alterar sua foto. Tente novamente.',
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    } finally {
+      setHasPhotoLoaded(true)
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -34,13 +87,13 @@ export function Profile() {
             isLoaded={hasPhotoLoaded}
           >
             <UserPhoto
-              source={{ uri: 'https://github.com/luismda.png' }}
+              source={{ uri: userPhoto }}
               alt="Luís Miguel"
               size={33}
             />
           </Skeleton>
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleSelectUserPhoto}>
             <Text
               color="green.500"
               fontWeight="bold"
