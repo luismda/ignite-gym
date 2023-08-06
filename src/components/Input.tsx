96d6f44acfaf401fdc122c4f4b529cd170cd2f4b
luismda <luismigueldutraalves@gmail.com>
2023-08-05 21:37:54 -0300

    feat: add form validation on sign up

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,33 +1,59 @@
-import { Input as NativeBaseInput, IInputProps, useTheme } from 'native-base'
+import {
+  Input as NativeBaseInput,
+  IInputProps,
+  FormControl,
+  useTheme,
+} from 'native-base'
 
-type InputProps = IInputProps
+interface InputProps extends IInputProps {
+  errorMessage?: string | null
+}
 
-export function Input(props: InputProps) {
+export function Input({ errorMessage = null, isInvalid, ...rest }: InputProps) {
   const { colors } = useTheme()
 
+  const isInvalidInput = !!errorMessage || isInvalid
+
   return (
-    <NativeBaseInput
-      bg="gray.700"
-      h={14}
-      w="full"
-      px={4}
-      mb={4}
-      borderWidth={0}
-      fontSize="md"
-      color="white"
-      fontFamily="body"
-      placeholderTextColor="gray.300"
-      cursorColor={colors.green[500]}
-      _focus={{
-        bg: 'gray.700',
-        borderWidth: 1,
-        borderColor: 'green.500',
-      }}
-      _disabled={{
-        bg: 'gray.600',
-        opacity: 0.5,
-      }}
-      {...props}
-    />
+    <FormControl isInvalid={isInvalidInput} mb={4}>
+      <NativeBaseInput
+        bg="gray.700"
+        h={14}
+        w="full"
+        px={4}
+        borderWidth={0}
+        fontSize="md"
+        color="white"
+        fontFamily="body"
+        cursorColor={colors.green[500]}
+        placeholderTextColor="gray.300"
+        isInvalid={isInvalidInput}
+        _invalid={{
+          borderWidth: 1,
+          borderColor: 'red.500',
+        }}
+        _focus={
+          isInvalidInput
+            ? {
+                borderWidth: 1,
+                borderColor: 'red.500',
+              }
+            : {
+                bg: 'gray.700',
+                borderWidth: 1,
+                borderColor: 'green.500',
+              }
+        }
+        _disabled={{
+          bg: 'gray.600',
+          opacity: 0.5,
+        }}
+        {...rest}
+      />
+
+      <FormControl.ErrorMessage _text={{ color: 'red.500' }}>
+        {errorMessage}
+      </FormControl.ErrorMessage>
+    </FormControl>
   )
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,8 @@
 import { useNavigation } from '@react-navigation/native'
 import { Center, Heading, Image, Text, VStack, ScrollView } from 'native-base'
+import { useForm, Controller } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
 import LogoSvg from '@assets/logo.svg'
 import backgroundImg from '@assets/background.png'
@@ -7,8 +10,50 @@ import backgroundImg from '@assets/background.png'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
+type SignUpFormData = {
+  name: string
+  email: string
+  password: string
+  confirmPassword: string
+}
+
+const signUpFormSchema = z
+  .object({
+    name: z
+      .string({ required_error: 'Informe o seu nome.' })
+      .trim()
+      .nonempty('Informe o seu nome.'),
+    email: z
+      .string({ required_error: 'Informe um e-mail.' })
+      .email('Informe um e-mail válido.'),
+    password: z
+      .string({ required_error: 'Informe uma senha.' })
+      .trim()
+      .min(6, 'A senha deve ter um mínimo de 6 dígitos.'),
+    confirmPassword: z
+      .string({ required_error: 'Confirme a senha.' })
+      .trim()
+      .nonempty('Confirme a senha.'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'A confirmação da senha não corresponde.',
+  })
+
 export function SignUp() {
   const navigation = useNavigation()
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpFormSchema),
+  })
+
+  function handleSignUp(data: SignUpFormData) {
+    console.log(data)
+  }
 
   function handleGoBack() {
     navigation.goBack()
@@ -41,23 +86,74 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input placeholder="Nome" />
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                placeholder="Nome"
+                errorMessage={errors.name?.message}
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
           />
 
-          <Input placeholder="Senha" secureTextEntry />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                errorMessage={errors.email?.message}
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
+          />
 
-          <Button title="Criar e acessar" />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                errorMessage={errors.password?.message}
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="confirmPassword"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                placeholder="Confirmar senha"
+                returnKeyType="send"
+                secureTextEntry
+                errorMessage={errors.confirmPassword?.message}
+                value={value}
+                onChangeText={onChange}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+              />
+            )}
+          />
+
+          <Button
+            title="Criar e acessar"
+            onPress={handleSubmit(handleSignUp)}
+          />
         </Center>
 
         <Button
           title="Voltar para o login"
           variant="outline"
-          mt={24}
+          mt={16}
           onPress={handleGoBack}
         />
       </VStack>
