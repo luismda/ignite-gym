1b57b45dd2fc4a0d470d7370ff129a4f9a816b0b
luismda <luismigueldutraalves@gmail.com>
2023-08-19 10:48:52 -0300

    feat: fetch history

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,6 +1,12 @@
 import { HStack, Heading, Text, VStack } from 'native-base'
 
-export function HistoryCard() {
+import { HistoryDTO } from '@dtos/HistoryDTO'
+
+interface HistoryCardProps {
+  data: HistoryDTO
+}
+
+export function HistoryCard({ data }: HistoryCardProps) {
   return (
     <HStack
       w="full"
@@ -20,16 +26,16 @@ export function HistoryCard() {
           textTransform="capitalize"
           numberOfLines={1}
         >
-          Costas
+          {data.group}
         </Heading>
 
         <Text color="gray.100" fontSize="lg" numberOfLines={1}>
-          Puxada frontal
+          {data.name}
         </Text>
       </VStack>
 
       <Text color="gray.300" fontSize="md">
-        08:56
+        {data.hour}
       </Text>
     </HStack>
   )
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,52 +1,88 @@
-import { useState } from 'react'
-import { VStack, SectionList, Heading, Text } from 'native-base'
+import { useCallback, useState } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import { VStack, SectionList, Heading, Text, useToast } from 'native-base'
 
-import { ScreenHeader } from '@components/ScreenHeader'
+import { AppError } from '@utils/AppError'
+
+import { api } from '@services/api'
+
+import { HistoryByDayDTO } from '@dtos/HistoryByDayDTO'
+
+import { Loading } from '@components/Loading'
 import { HistoryCard } from '@components/HistoryCard'
+import { ScreenHeader } from '@components/ScreenHeader'
 
 export function History() {
-  const [exercises, setExercises] = useState([
-    {
-      title: '29.07.23',
-      data: ['Puxada frontal', 'Remada unilateral'],
-    },
-    {
-      title: '28.07.23',
-      data: ['Puxada frontal'],
-    },
-  ])
+  const [isLoading, setIsLoading] = useState(true)
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([])
+
+  const toast = useToast()
+
+  async function fetchHistory() {
+    setIsLoading(true)
+
+    try {
+      const response = await api.get('/history')
+
+      setExercises(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+
+      const title = isAppError
+        ? error.message
+        : 'Falha ao carregar seu histórico.'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchHistory()
+    }, []),
+  )
 
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
 
-      <SectionList
-        sections={exercises}
-        keyExtractor={(item) => item}
-        renderItem={() => <HistoryCard />}
-        renderSectionHeader={({ section }) => (
-          <Heading
-            color="gray.200"
-            fontSize="md"
-            fontFamily="heading"
-            mt={10}
-            mb={3}
-          >
-            {section.title}
-          </Heading>
-        )}
-        ListEmptyComponent={() => (
-          <Text color="gray.100" textAlign="center">
-            Não há exercícios registrados ainda. {'\n'}
-            Que tal fazer um exercício hoje?
-          </Text>
-        )}
-        contentContainerStyle={
-          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
-        }
-        showsVerticalScrollIndicator={false}
-        px={8}
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <SectionList
+          sections={exercises}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <HistoryCard data={item} />}
+          renderSectionHeader={({ section }) => (
+            <Heading
+              color="gray.200"
+              fontSize="md"
+              fontFamily="heading"
+              mt={10}
+              mb={3}
+            >
+              {section.title}
+            </Heading>
+          )}
+          ListEmptyComponent={() => (
+            <Text color="gray.100" textAlign="center">
+              Não há exercícios registrados ainda. {'\n'}
+              Que tal fazer um exercício hoje?
+            </Text>
+          )}
+          contentContainerStyle={
+            exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+          }
+          showsVerticalScrollIndicator={false}
+          px={8}
+        />
+      )}
     </VStack>
   )
 }
